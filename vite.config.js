3e232b815e331341b3ef678ae5b36168b5cce92d
2899053483 <Liu2899053483@gmail.com>
2025-12-27 04:19:41 +0800

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dengue-pred-ai/', 
   plugins: [react()],
+  base: '/dengue-pred-app/', // <--- 加上这一行！注意前后都要有斜杠
 })
